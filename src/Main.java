c10a250690a2ffddfef7cf1ae0b14944cc54aeaa
Long Nguyen <n.h.long.9697@gmail.com>
2020-07-28 11:26:40 +0700

    update name

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("this is bao branch");
-        System.out.println("my name is bao");
+        System.out.println("this is bao branch update");
+        System.out.println("my name is bao update");
     }
 }
