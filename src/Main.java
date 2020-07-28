ca5aeab0316156ddbcc467e2ef74c9f1ea8c0fb0
Long Nguyen <n.h.long.9697@gmail.com>
2020-07-28 09:30:45 +0700

    update master

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("this is master branch");
-        System.out.println("my name is master");
+        System.out.println("this is master branch update");
+        System.out.println("my name is master update");
     }
 }
