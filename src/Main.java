32ea5bff7bd90ee636f02a859f6c70858179f150
Long Nguyen <n.h.long.9697@gmail.com>
2020-07-28 09:30:11 +0700

    change name

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("this is master branch");
-        System.out.println("my name is master");
+        System.out.println("this is long branch");
+        System.out.println("my name is long");
     }
 }
