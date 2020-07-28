4152592da054cc9e1655b04b551dbbda902d8c8d
Long Nguyen <n.h.long.9697@gmail.com>
2020-07-28 10:55:12 +0700

    change to long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("this is master branch update");
-        System.out.println("my name is master update");
+        System.out.println("this is long");
+        System.out.println("my name is long");
     }
 }
