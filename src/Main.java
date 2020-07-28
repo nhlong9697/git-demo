7b9012855718abad7ed7a5b485cf9a643228c31b
Long Nguyen <n.h.long.9697@gmail.com>
2020-07-28 09:22:43 +0700

    add for loop

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("this is bao branch");
-        System.out.println("my name is bao");
+        System.out.println("this is long branch");
+        System.out.println("my name is long");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
