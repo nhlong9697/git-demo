a4628af26873bed3b2b53e367241ba2173e61a2d
Long Nguyen <n.h.long.9697@gmail.com>
2020-07-28 10:18:50 +0700

    change to bao

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("this is master branch update");
-        System.out.println("my name is master update");
+        System.out.println("this is bao branch");
+        System.out.println("my name is bao");
     }
 }
